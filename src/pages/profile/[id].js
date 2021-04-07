25c33c978304ae881b62c26c5f6bf7bdec8f7a87
ggenilson <genilsonaraujo20002002@gmail.com>
2021-04-07 06:58:03 +0100

    updates

--- src/pages/profile/[id].js
+++ src/pages/profile/[id].js
@@ -1,24 +1,47 @@
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 import axios from 'axios';
 
-function Profile() {
-  const { query } = useRouter();
+function Profile({ user = {} }) {
+  //   const { query } = useRouter();
 
-  return <h1>Profile {query.id}</h1>;
+  return (
+    <div>
+      <p>{user.id}</p>
+      <p>{user.name}</p>
+      <p>{user.username}</p>
+    </div>
+  );
 }
 
-export async function getStaticProps({ params }) {
-  console.log('params: ', params);
-
+export async function getStaticProps(context) {
   const response = await axios.get(
     'https://jsonplaceholder.typicode.com/users',
-    { params: { id: params.id } }
+    { params: { id: context.params.id } }
   );
 
   const user = response.data[0];
 
   return {
     props: { user },
+  };
+}
+
+export async function getStaticPaths() {
+  const response = await axios.get(
+    'https://jsonplaceholder.typicode.com/users'
+  );
+
+  const users = response.data;
+
+  const paths = users.map(({ id }) => {
+    return {
+      params: { id: String(id) },
+    };
+  });
+
+  return {
+    paths,
+    fallback: true,
   };
 }
 
