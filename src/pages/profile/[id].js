9f6f6c391a7eeedb5bfa53f6869c5f0f988d62d3
ggenilson <genilsonaraujo20002002@gmail.com>
2021-04-07 06:39:46 +0100

    updates

--- src/pages/profile/[id].js
+++ src/pages/profile/[id].js
@@ -1,5 +1,25 @@
+import { useRouter } from 'next/router';
+import axios from 'axios';
+
 function Profile() {
-  return <h1>Profile</h1>;
+  const { query } = useRouter();
+
+  return <h1>Profile {query.id}</h1>;
+}
+
+export async function getStaticProps({ params }) {
+  console.log('params: ', params);
+
+  const response = await axios.get(
+    'https://jsonplaceholder.typicode.com/users',
+    { params: { id: params.id } }
+  );
+
+  const user = response.data[0];
+
+  return {
+    props: { user },
+  };
 }
 
 export default Profile;
